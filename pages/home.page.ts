bfe184e496c090f7cf9a21034fb1f5b9752ef463
Selvaraj Kuppusamy <selvarajkuppusamy.29@gmail.com>
2023-09-02 21:37:09 +0100

    Fix broken tests

--- pages/home.page.ts
+++ pages/home.page.ts
@@ -15,9 +15,9 @@ class HomePage {
     constructor(page: Page) { 
         this.page = page;
         this.getStartedBtn = page.locator('#get-started');
-        this.headingText = page.locator('text=think different. make different.');           // ' ' is case in-sensitive, " " is case Sensitive.
-        // this.headingText = page.getByText('think different. make different.');           // works too
-        this.homeMenu = page.locator('#primary-menu >> text=Home');                         // >> lets adding 2 attributes
+        // this.headingText = page.locator('text=think different. make different.');           // ' ' is case in-sensitive, " " is case Sensitive.
+        this.headingText = page.getByText('think different. make different.');           // works too
+        this.homeMenu = page.locator('#zak-primary-menu >> text=Home');                         // >> lets adding 2 attributes
         // this.homeMenu = page.locator('#primary-menu:has-text("Home")');                  // Works too
         this.searchIcon = page.locator('//*[@class="zak-header-actions zak-header-actions--desktop"]//*[@class="zak-header-search__toggle"]');
         this.menuNames = page.locator('#zak-primary-nav li[id*=menu]');
